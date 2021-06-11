2aff7556910861f3cd0e0ca06eb9f5628f608470
Divij Gera <divijgera97@gmail.com>
2021-06-11 15:48:07 +0530

    test runner reports

--- src/test/Step_Definitions/TestRunner.java
+++ src/test/Step_Definitions/TestRunner.java
@@ -7,6 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/Features",
-        glue = {"src/test/Step_Definitions"})
+        glue = {"src/test/Step_Definitions"},
+monochrome = true,
+plugin = {"pretty","junit:target/JUnitReport/junit_report.xml",
+"json:target/JSONReport/json_report.json",
+"html:target/HTMLReport/html_report.html"})
+
 public class TestRunner {
 }
